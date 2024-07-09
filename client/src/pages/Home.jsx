2f4ed979a4cc7a112ac70de104bac42c2c75a7cf
devgupta4526 <devguptamsi@gmail.com>
2024-07-09 13:58:54 +0530

    Created Navbar SearchUI HeroSection

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 
 const Home = () => {
   return (
-    <>
-
+    <div className='mt-36'>
+    <HeroSection/>
     <div>Home</div>
-    </>
+    </div>
   )
 }
 
