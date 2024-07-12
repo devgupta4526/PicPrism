7392092855a4bfd219adc338fae773188f04140f
devgupta4526 <devguptamsi@gmail.com>
2024-07-12 20:35:30 +0530

    Dyanmic Routing and DB Connectivity added

--- server/index.js
+++ server/index.js
@@ -1,15 +1,28 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const {readdirSync} = require("fs")
 
 const app = express()
+const authRoute = require("./routes/auth.route")
+const connectDB = require("./db/connection")
 
 dotenv.config()
 
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT || 5000;
+connectDB()
+
+readdirSync("./routes").map((route)=>{
+    app.use("/api",require(`./routes/${route}`))
+})
+
 app.get("/", (req,res) =>{
     res.send("Hello");
 })
+
+
+
+
 
 app.listen(port,() =>{
     console.log(`Server is started!!!! Port : ${port}`);
---
+++ server/routes/auth.route.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+
+
+router.get("/hello", (req,res) =>{
+    res.json({
+        name: "Dev Gupta"
+    })
+})
+
+
+module.exports = router;
