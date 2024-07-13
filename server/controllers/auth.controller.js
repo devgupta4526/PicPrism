b1dabea65dee1f5a6384aafa145e830523829dc1
devgupta4526 <devguptamsi@gmail.com>
2024-07-13 20:20:15 +0530

    Access Token Refresh Token Verify Token  and Login functionality added

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,36 +1,71 @@
-const User = require("../models/user.model")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const generateAccessToken = require("../utils/accesstoken");
+const generateRefreshToken = require("../utils/refreshToken");
 
+const signup = async (req, res) => {
+  const { username, email, password, accountType } = req.body;
+  try {
+    let user = await User.findOne({ username: username });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Username Already exist" });
+    }
+    const securePassword = await bcrypt.hash(password, 10);
+    user = new User({
+      username,
+      email,
+      password: securePassword,
+      accountType,
+    });
 
-const signup = async (req,res) => {
-    const {username,email,password,accountType} = req.body;
-    try {
-       let user = await User.findOne({username : username}); 
-       if(user){
-        return res.status(400).json({success: failed , message : "Username Already exist"})
-       }
-       const securePassword = await bcrypt.hash(password, 10);
-       user = new User({
-        username,
-        email,
-        password: securePassword,
-        accountType,
-       });
+    await user.save();
 
-       await user.save();
+    return res.status(201).json({ success: true, message: "User Created" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-       return res.status(201).json({success : true, message: "User Created"});
-
-        
-    } catch (error) {
-        return res.status(500).json({success: false, message: error.message});
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+    }
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid Credentials" });
     }
 
+    const data = {
+      id: user._id,
+      accountType: user.accountType,
+      author: user.username
+    };
+
+    const accessToken = generateAccessToken(data);
+    const refreshToken = generateRefreshToken(data);
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged in",
+      accessToken : accessToken,
+      refreshToken : refreshToken,
+      role: user.accountType,
+      author: user.username
+    });
+
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
 };
 
-const login = async (req, res) =>{
-
-};
-
-module.exports = {login, signup};
+module.exports = { login, signup };
